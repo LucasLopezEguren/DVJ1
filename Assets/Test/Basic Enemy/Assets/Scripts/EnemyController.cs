d2385b1de9d10f0b0cbd7d3f2271f03153a04119
Carlos Andrés Chiale <carlos.chiale@hotmail.com>
2020-09-22 00:46:45 -0300

    Enemy follows player

--- Assets/Test/Basic Enemy/Assets/Scripts/EnemyController.cs
+++ Assets/Test/Basic Enemy/Assets/Scripts/EnemyController.cs
@@ -6,9 +6,7 @@ public class EnemyController : MonoBehaviour
 {
     private Transform targetPlayer;
 
-    public float movementSpeed = 8.0f;
-
-    public float distance = 2.0f;
+    public float movementSpeed = 6.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +17,8 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, targetPlayer.position) >= distance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, movementSpeed * Time.deltaTime);
-        }      
+        transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, movementSpeed * Time.deltaTime);
+
         if (transform.position.x < targetPlayer.position.x)
         {
             transform.localScale = new Vector3(1, 1, 1);
