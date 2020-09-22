70fd288642920e73989be4827cc7434e1229be87
Carlos Andrés Chiale <carlos.chiale@hotmail.com>
2020-09-22 00:46:45 -0300

    Enemy follows player

---
+++ Assets/Test/Basic Enemy/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Transform targetPlayer;
+
+    public float movementSpeed = 6.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, movementSpeed * Time.deltaTime);
+
+        if (transform.position.x < targetPlayer.position.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+}
