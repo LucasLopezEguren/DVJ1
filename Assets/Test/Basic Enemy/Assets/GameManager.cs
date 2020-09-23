fdf4bc35fd2d28b290698c9d2f61170a4eefa7c7
Lucas Lopez <lucaslopezeguren@gmail.com>
2020-09-23 19:32:04 -0300

    seteo de variables y remove de debugs

--- Assets/Test/Basic Enemy/Assets/GameManager.cs
+++ Assets/Test/Basic Enemy/Assets/GameManager.cs
@@ -12,8 +12,6 @@ public class GameManager : MonoBehaviour
         int amountChunksTypes = levelChunks.Length;
         for (int i = 0; i <= amountChunks; i++) {
             GameObject toInitiatie = levelChunks[Mathf.FloorToInt(UnityEngine.Random.Range(0f, Mathf.Round(amountChunksTypes)))];
-            Debug.Log(toInitiatie.GetComponentInChildren<Collider2D>().bounds.size);
-            Debug.Log(i*10);
             Instantiate(toInitiatie, new Vector3(i*50, 0, 0), Quaternion.identity);
         }
     }
