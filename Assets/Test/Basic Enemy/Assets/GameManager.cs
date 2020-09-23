26c9827cc41462b7970c4f1c9694175cb5d01ed7
Lucas Lopez <lucaslopezeguren@gmail.com>
2020-09-23 16:23:17 -0300

    add basic for a random level generetor

--- Assets/Test/Basic Enemy/Assets/GameManager.cs
+++ Assets/Test/Basic Enemy/Assets/GameManager.cs
@@ -12,6 +12,8 @@ public class GameManager : MonoBehaviour
         int amountChunksTypes = levelChunks.Length;
         for (int i = 0; i <= amountChunks; i++) {
             GameObject toInitiatie = levelChunks[Mathf.FloorToInt(UnityEngine.Random.Range(0f, Mathf.Round(amountChunksTypes)))];
+            Debug.Log(toInitiatie.GetComponentInChildren<Collider2D>().bounds.size);
+            Debug.Log(i*10);
             Instantiate(toInitiatie, new Vector3(i*50, 0, 0), Quaternion.identity);
         }
     }
