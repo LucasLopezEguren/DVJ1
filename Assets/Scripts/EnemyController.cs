1d97cb6c7696b8f62467734a8764bfe980cfd6c0
Carlos Andrés Chiale <carlos.chiale@hotmail.com>
2020-10-01 14:17:57 -0300

    Enemy follow player when it's in range

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,9 +6,9 @@ public class EnemyController : MonoBehaviour
 {
     private Transform targetPlayer;
 
-    public float movementSpeed = 2.0f;
+    public float movementSpeed = 5.0f;
 
-    public float distance = 2.0f;
+    public float distance = 1.0f;
 
     public float life = 100.0f;
 
@@ -17,6 +17,10 @@ public class EnemyController : MonoBehaviour
     public bool isFacingRight = false;
 
     public bool isWalking;
+
+    private bool isChasing = false;
+
+    public float rangeForChasing = 5.0f;
 
     private Rigidbody rb;
 
@@ -33,13 +37,42 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckMovementDirection();
+        CheckMovement();
         UpdateAnimations();
-        if (Vector3.Distance(transform.position, targetPlayer.position) >= distance)
+        CheckDirection(transform.position.x, targetPlayer.position.x);
+        CheckStartChasing();
+        if (isChasing && Vector3.Distance(transform.position, targetPlayer.position) >= distance)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPlayer.position, movementSpeed * Time.deltaTime);
         }
-        if (transform.position.x < targetPlayer.position.x)
+    }
+
+    private void CheckStartChasing()
+    {
+        if (Vector3.Distance(transform.position, targetPlayer.position) <= rangeForChasing) isChasing = true;
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void CheckMovement()
+    {
+        if (rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+
+    private void CheckDirection(float positionX, float targetPositionX)
+    {
+        if (positionX < targetPositionX)
         {
             transform.localScale = new Vector3(1, 1, 1);
             if (!isFacingRight)
@@ -55,24 +88,6 @@ public class EnemyController : MonoBehaviour
                 Flip();
             }
             isFacingRight = false;
-        }
-    }
-
-    private void Flip()
-    {
-        isFacingRight = !isFacingRight;
-        transform.Rotate(0.0f, 180.0f, 0.0f);
-    }
-
-    private void CheckMovementDirection()
-    {
-        if (rb.velocity.x != 0)
-        {
-            isWalking = true;
-        }
-        else
-        {
-            isWalking = false;
         }
     }
 
