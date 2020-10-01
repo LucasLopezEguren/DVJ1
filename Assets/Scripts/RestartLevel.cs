0041982ce05c71f4aadbe289466c7eaa42375376
Lucas Lopez <lucaslopezeguren@gmail.com>
2020-10-01 17:27:27 -0300

    se lleva al hub al caerte de la plataforma movil

---
+++ Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class RestartLevel : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+        SceneManager.LoadScene("hub");
+    }
+}
