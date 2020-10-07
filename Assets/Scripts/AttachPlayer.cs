8fd98da394bc4ba086efb8e1b01bfe8a3506518c
Lucas Lopez <lucaslopezeguren@gmail.com>
2020-10-07 11:13:26 -0300

    fix para que las armas no pierda la hitbox al subir a la plataforma

--- Assets/Scripts/AttachPlayer.cs
+++ Assets/Scripts/AttachPlayer.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class AttachPlayer : MonoBehaviour{
 
     public GameObject player;
+    public List<GameObject> weapons;
 
     void Start () {
         player = GameObject.Find("Player");
+        foreach (GameObject weapon in GameObject.FindGameObjectsWithTag("Weapon")){
+            weapons.Add(weapon);
+        }
     }
 
     void OnTriggerEnter(Collider other) {
+        if (!weapons.Contains(other.gameObject)) {
             other.gameObject.transform.parent = transform;
+        }
     }
     void OnTriggerExit(Collider other) {
+        if (!weapons.Contains(other.gameObject)) {
             other.gameObject.transform.parent = null;
-        
+        }
     }
 }
