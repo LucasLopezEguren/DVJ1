dfa5d39d24edabd1c577e55c6473768bd4a89c99
Lucas Lopez <lucaslopezeguren@gmail.com>
2020-10-17 19:51:19 -0300

    create combo counter

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject[] levelChunks;
     public int amountChunks;
+    public Text comboNumber;
+    public Text comboText;
+    private Color transparencyNumber;
+    private Color transparencyText;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +24,45 @@ public class GameManager : MonoBehaviour
             Instantiate(toInitiatie, new Vector3(nextPosition, 0, 0), Quaternion.identity);
             nextPosition = nextPosition + toInitiatie.GetComponent<Transform>().localScale.x;
         }
-    }
+        comboNumber.text = "";
+        comboText.text = "";
+        transparencyNumber = comboNumber.color;
+        transparencyNumber.a = 0.0f;
+        transparencyText = comboText.color;
+        transparencyText.a = 0.0f;
 
+    }
+    private float comboCountLifeTime = 3;
+    private float comboCurrentTime = 0;
+    private int comboCount = 0;
     // Update is called once per frame
+    private float transparency;
     void Update()
     {
-        
+        if (comboCount > 3 && comboCurrentTime > comboCountLifeTime) {
+            comboCount = 0;
+            comboCurrentTime = 0f;
+        }
+        if (comboCount > 3) {
+            transparency = (comboCountLifeTime - comboCurrentTime)/comboCountLifeTime;
+            Debug.Log("comboCurrentTime: " + comboCurrentTime);
+            Debug.Log("transparency: " + transparency);  
+            Debug.Log("combo: " + comboCount);  
+            comboNumber.text = comboCount.ToString();
+            comboText.text = "hits";
+            transparencyNumber.a = transparency;
+            transparencyText.a = transparency;
+            comboNumber.color = transparencyNumber;
+            comboText.color = transparencyText;
+        }
+        if (comboCount > 0) {
+            comboCurrentTime = comboCurrentTime + Time.deltaTime;
+        }
+    }
+
+    public void addComboHit () {
+        Debug.Log("comboCount: " + comboCount);
+        comboCount++;
+        comboCurrentTime = 0f;
     }
 }
