9a4d964c04bfceecf82e25a41ebf89b0077aeeba
dsuarez <dsuarez_18@hotmail.com>
2020-10-21 20:04:35 -0300

    enemigo volador con bomba

---
+++ Assets/Scripts/FEShooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FEShooting : MonoBehaviour
+{
+    public float bulletSpeed = 5f;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(-bulletSpeed, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, 3);
+    }
+}
---
+++ Assets/Scripts/FlyingEnemyController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyController : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+
+    public int health;
+
+    public int maxHealth = 100;
+
+    public bool isWalking;
+
+    private Rigidbody rb;
+
+    public GameManager gameManager;
+
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(-movementSpeed, 0, 0, ForceMode.Impulse);
+        health = maxHealth;
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        InvokeRepeating(nameof(ShootBullet), 0f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMovement();
+    }
+
+    private void CheckMovement()
+    {
+        if (rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+
+    public void ShootBullet()
+    {
+        GameObject b = Instantiate(bullet);
+        b.transform.position = transform.position;
+
+    }
+}
