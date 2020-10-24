d7f1109ae7a7bc83f7641fa6d85d6b23b2c19237
dsuarez <dsuarez_18@hotmail.com>
2020-10-24 18:59:46 -0300

    daño de bomba al jugador

--- Assets/Scripts/FEBombDrop.cs
+++ Assets/Scripts/FEBombDrop.cs
@@ -6,7 +6,13 @@ public class FEBombDrop : MonoBehaviour
 {
     public float bombSpeed = 5f;
 
+    public float explosionRadius = 1f;
+
+    public bool hasExploded = false;
+
     private Rigidbody rb;
+
+    public GameObject explosionEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +25,30 @@ public class FEBombDrop : MonoBehaviour
     void Update()
     {
         Destroy(gameObject, 3);
+    }
+
+    void OnCollisionEnter(Collision collided)
+    {
+        if (!hasExploded)
+        {
+            hasExploded = true;
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            if(nearbyObject.gameObject.tag == "Player")
+            {
+                nearbyObject.GetComponent<PlayerController>().takeDamage(10);
+            }
+        }
+        Destroy(gameObject);
     }
 }
