2c4477c8e44941c5c3b284f469aebdb513e717bb
Nico <nquintana11cb@gmail.com>
2020-10-27 15:41:49 -0300

    range attack done

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody rb;
+    public int damage = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter (Collider hitEnemy)
+    {
+        hitEnemy.GetComponent<DamageController>().TakeDamage(damage);
+        /*Enemy enemy = other.GetComponent<Enemy>();
+        if(enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }*/
+        Destroy(gameObject);
+    }
+
+}
