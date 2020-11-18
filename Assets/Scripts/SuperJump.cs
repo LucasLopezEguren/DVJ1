18116e17f63ebb51e1527c49fd5a2a064edd4926
Lucas Lopez <lucaslopezeguren@gmail.com>
2020-11-18 20:57:25 -0300

    cambia la forma de obtener los valor de salto y corrida

--- Assets/Scripts/SuperJump.cs
+++ Assets/Scripts/SuperJump.cs
@@ -7,12 +7,16 @@ public class SuperJump : MonoBehaviour
     private float enterX;
     private float enterY;
     private int playerCurrentHp;
+    private float jumpForce;
+    private float moveSpeed;
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player"){            
             PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
             playerCurrentHp = playerController.currentHealth;
             enterX = other.transform.position.x;
             enterY = other.transform.position.y;
+            jumpForce = playerController.jumpForce;
+            moveSpeed = playerController.moveSpeed;
             playerController.jumpForce = 35f;
             playerController.moveSpeed = 25f;
         }
@@ -21,8 +25,8 @@ public class SuperJump : MonoBehaviour
     void OnTriggerExit(Collider other) {
         if (other.tag == "Player"){            
             PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
-            playerController.jumpForce = 15f;
-            playerController.moveSpeed = 10f;
+            playerController.jumpForce = jumpForce;
+            playerController.moveSpeed = moveSpeed;
             if (playerController.currentHealth >= playerCurrentHp && enterX < other.transform.position.x && enterY < other.transform.position.y) {
                 FindObjectOfType<AudioManager>().Play("SuperNova");
             }
