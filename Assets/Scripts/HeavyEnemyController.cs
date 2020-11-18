e98a8139ddcde52c2ddf007a65b511829f7e2eb9
dsuarez <dsuarez_18@hotmail.com>
2020-11-18 19:40:58 -0300

    basic heavy enemy

---
+++ Assets/Scripts/HeavyEnemyController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HeavyEnemyController : MonoBehaviour
+{
+    public float movementSpeed = 3.0f;
+
+    public float rangeForChasing = 5f;
+
+    public float attackRange = 1f;
+
+    private Rigidbody rb;
+
+    public GameObject healthBarUI;
+
+    public Slider slider;
+
+    private DamageController damageController;
+
+    public GameObject bloodSplash;
+
+    private Transform targetPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        damageController = this.GetComponent<DamageController>();
+        targetPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
