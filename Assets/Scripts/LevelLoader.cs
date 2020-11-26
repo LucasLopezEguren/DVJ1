745c4ff1bc5c10cc82513342d7253a997caa36e1
Carlos Andrés Chiale <carlos.chiale@hotmail.com>
2020-11-26 18:22:12 -0300

    Improve transitions script

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public enum Transitions
+    {
+        Crossfade,
+        CircleWipe
+    }
+
+    public Transitions transition;
+
+    public float transitionTime = 1f;
+
+    private Animator transitionAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (transition)
+        {
+            case Transitions.Crossfade:
+                transform.Find("Crossfade").gameObject.SetActive(true);
+                transitionAnim = transform.Find("Crossfade").gameObject.GetComponent<Animator>();
+                break;
+            case Transitions.CircleWipe:
+                transform.Find("CircleWipe").gameObject.SetActive(true);
+                transitionAnim = transform.Find("CircleWipe").gameObject.GetComponent<Animator>();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadNextLevel(string scene)
+    {
+        StartCoroutine(LoadScene(scene));
+    }
+
+    IEnumerator LoadScene(string scene)
+    {
+        transitionAnim.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(scene);
+    }
+
+}
