9fa9f69b5f4f48b1a7e01d8513f94497b843b9b7
dsuarez <dsuarez_18@hotmail.com>
2020-12-02 20:46:18 -0300

    Basic Heavy Enemy

--- Assets/Scripts/HeavyEnemyController.cs
+++ Assets/Scripts/HeavyEnemyController.cs
@@ -5,15 +5,31 @@ public class HeavyEnemyController : MonoBehaviour
 {
     public float movementSpeed = 3.0f;
 
+    public float chasingSpeed = 5.0f;
+
     public float rangeForChasing = 5f;
+
+    public float maxMovementRange = 5f;
 
     public float attackRange = 1f;
 
+    private bool isChasing = false;
+
+    private bool isAttacking = false;
+
+    public bool isFacingRight = false;
+
+    public Vector3 initialPosition;
+
+    public Vector3 minPosition;
+
+    public Vector3 maxPosition;
+
+    public Animator anim;
+
+    public CheckEdge checkEdge;
+
     private Rigidbody rb;
-
-    public GameObject healthBarUI;
-
-    public Slider slider;
 
     private DamageController damageController;
 
@@ -25,13 +41,109 @@ public class HeavyEnemyController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        initialPosition = transform.position;
+        minPosition = new Vector3(initialPosition.x - maxMovementRange, initialPosition.y, initialPosition.z);
+        maxPosition = new Vector3(initialPosition.x + maxMovementRange, initialPosition.y, initialPosition.z);
         damageController = this.GetComponent<DamageController>();
         targetPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        anim.SetBool("isDying", false);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //UpdateAnimations();
+        Movement();
+        /*if (!IsNearEdge() && IsAlive())
+        {
+            if (isChasing && Vector3.Distance(transform.position, targetPlayer.position) >= attackRange && !anim.GetCurrentAnimatorStateInfo(0).IsName("Ataque1"))
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPlayer.position, chasingSpeed * Time.deltaTime);
+            }
+            else
+            {
+                if (isChasing && Vector3.Distance(transform.position, targetPlayer.position) < attackRange)
+                {
+                    isChasing = false;
+                    isAttacking = true;
+                }
+            }
+        }*/
+    }
+
+    private bool IsNearEdge()
+    {
+        return checkEdge.isNearEdge;
+    }
+
+    private void UpdateAnimations()
+    {
+        anim.SetBool("isChasing", isChasing && !IsNearEdge());
+    }
+
+    private float CalculateHealth()
+    {
+        return damageController.health;
+    }
+
+    private bool IsAlive()
+    {
+        return CalculateHealth() > 0;
+    }
+
+    private void Movement()
+    {
+        if (IsAlive())
+        {
+            //CheckStartChasing();
+            if (!isChasing)
+            {
+                if (!isFacingRight)
+                {
+                    if (Vector3.Distance(transform.position, minPosition) > 0f)
+                    {
+                        transform.position = Vector3.MoveTowards(transform.position, minPosition, movementSpeed * Time.deltaTime);
+                    }
+                    else
+                    {
+                        Flip();
+                    }
+
+                }
+                else
+                {
+                    if (Vector3.Distance(transform.position, maxPosition) > 0f)
+                    {
+                        transform.position = Vector3.MoveTowards(transform.position, maxPosition, movementSpeed * Time.deltaTime);
+                    }
+                    else
+                    {
+                        Flip();
+                    }
+                }
+            }
+            else
+            {
+                if (!isFacingRight)
+                {
+
+                }
+            }
+        }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void CheckStartChasing()
+    {
+        if (Vector3.Distance(transform.position, targetPlayer.position) <= rangeForChasing)
+        {
+            isAttacking = false;
+            isChasing = true;
+        }
     }
 }
