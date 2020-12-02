b8278a254c938126773caa3c43c911e772529fb4
Carlos Andrés Chiale <carlos.chiale@hotmail.com>
2020-12-02 15:52:41 -0300

    Trying with an indicator in TransporterChunk

--- Assets/Scripts/ActivePortal.cs
+++ Assets/Scripts/ActivePortal.cs
@@ -7,6 +7,7 @@ public class ActivePortal : MonoBehaviour
     public GameObject Portal;
     public GameObject forceFieldBack;
     public GameObject forceFieldFront;
+    public GameObject UIChunk;
     public bool triggederd = false;
 
     void OnTriggerEnter(Collider other) {
@@ -14,6 +15,7 @@ public class ActivePortal : MonoBehaviour
             Portal.SetActive(true);
             forceFieldBack.SetActive(true);
             forceFieldFront.SetActive(true);
+            UIChunk.SetActive(true);
             triggederd = true;
         }
     }
