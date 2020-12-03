14aa45f53c66c99f21850ccfd5f9a7adaa2c0868
Lucas Lopez <lucaslopezeguren@gmail.com>
2020-12-02 21:23:18 -0300

    fix super jump

--- Assets/Scripts/SuperJump.cs
+++ Assets/Scripts/SuperJump.cs
@@ -9,23 +9,34 @@ public class SuperJump : MonoBehaviour
     private int playerCurrentHp;
     private float jumpForce;
     private float moveSpeed;
+    private PlayerController playerController;
     void Start() {
-            PlayerController playerController = GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<PlayerController>();
+        playerController = GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<PlayerController>();
         if ( jumpForce != null) {
                 jumpForce = playerController.jumpForce;
                 moveSpeed = playerController.moveSpeed;
+        }
+    }
+
+    private bool addSpeed;
+    void FixedUpdate() {
+        if (addSpeed) {
+            if (playerController.jumpForce < 35) {
+                playerController.jumpForce += 0.5f;
             }
+            if (playerController.moveSpeed <= 12) {
+                playerController.moveSpeed += 0.5f;
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player"){            
-            PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
+            playerController = other.gameObject.GetComponent<PlayerController>();
             playerCurrentHp = playerController.currentHealth;
             enterX = other.transform.position.x;
             enterY = other.transform.position.y;
-            
-            playerController.jumpForce = 35f;
-            playerController.moveSpeed = 25f;
+            addSpeed = true;
         }
     }
 
@@ -34,6 +45,7 @@ public class SuperJump : MonoBehaviour
             PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
             playerController.jumpForce = jumpForce;
             playerController.moveSpeed = moveSpeed;
+            addSpeed = false;
             if (playerController.currentHealth >= playerCurrentHp && enterX < other.transform.position.x && enterY < other.transform.position.y) {
                 FindObjectOfType<AudioManager>().Play("SuperNova");
             }
