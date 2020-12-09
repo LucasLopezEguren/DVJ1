20e1a76c474b99c8e469cbfa8858eb87befd215a
Carlos Andrés Chiale <carlos.chiale@hotmail.com>
2020-12-09 18:41:04 -0300

    Fix in Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -30,4 +30,8 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+    public void Feedback()
+    {
+        Application.OpenURL("https://forms.gle/3o1fGK1hvSGvCsNL8");
+    }
 }
