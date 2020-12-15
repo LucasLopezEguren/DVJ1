ad8ce929eb322a2912d6d3f053cc4c32116ae2b3
Carlos Andrés Chiale <carlos.chiale@hotmail.com>
2020-12-15 16:24:37 -0300

    Starting with skilltree UI

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+
+    public GameObject manageSkillsMessage;
+
+    public GameObject x1;
+
+    public GameObject x2;
+
+    public GameObject SkillTreeUI;
+
+    private GameObject player;
+
+    private bool SkillTreeUIIsActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((player.transform.position.x > x1.transform.position.x && player.transform.position.x < x2.transform.position.x) && !SkillTreeUIIsActive)
+        {
+            manageSkillsMessage.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SkillTreeUIIsActive = true;
+                manageSkillsMessage.SetActive(false);
+                SkillTreeUI.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+        else
+        {
+            manageSkillsMessage.SetActive(false);
+        }
+    }
+
+    public void BackFromSkilltree()
+    {
+        Time.timeScale = 1f;
+        SkillTreeUIIsActive = false;
+        SkillTreeUI.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/SkillTreeUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTreeUI : MonoBehaviour
+{
+    private SkillTree skillTree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skillTree = (SkillTree)GameObject.Find("SkillTree").GetComponent("SkillTree");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
